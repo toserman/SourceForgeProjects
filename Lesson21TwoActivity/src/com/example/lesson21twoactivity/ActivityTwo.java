2cb27b35f65abcf67f4c213443dfc207e9018ffd
Anton <anton@cambium.synapse.com>
2013-08-15 13:52:38 +0300

    Create 2 screen

---
+++ Lesson21TwoActivity/src/com/example/lesson21twoactivity/ActivityTwo.java
@@ -0,0 +1,14 @@
+package com.example.lesson21twoactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.twoscreen);
+	}
+
+}
---
+++ Lesson21TwoActivity/src/com/example/lesson21twoactivity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lesson21twoactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+  Button btnActTwo;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    btnActTwo = (Button) findViewById(R.id.btnActTwo);
+    btnActTwo.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+    case R.id.btnActTwo:
+      // TODO Call second activity
+    	Intent intent = new Intent(this,ActivityTwo.class);
+    	startActivity(intent);
+    	
+      break;
+    default:
+      break;
+    }
+  }
+}
