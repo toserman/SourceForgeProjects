6578588aac70a5a5302ce93706cf933fd29453eb
Anton <anton@cambium.synapse.com>
2013-10-04 19:27:35 +0300

    Add new icon. Template with a few activity

--- WiFi/src/com/example/wifi/ActivityTwo.java
+++ WiFi/src/com/example/wifi/ActivityTwo.java
@@ -1,15 +1,39 @@
 package com.example.wifi;
 
+
+
+import java.io.Serializable;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+//Deneme dene = new Deneme(4,"Mustafa");
+//Intent i = new Intent(this, Y.class);
+//i.putExtra("sampleObject", dene);
 
 public class ActivityTwo extends Activity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_two);
-  }
+	public static TextView rowtablename,rowssid_1,rowssid_2,rowssid_3;
+	public void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.activity_two);	      
+//	      rowtablename = (TextView)findViewById(R.id.textTableName);	    
+	      rowssid_1 = (TextView)findViewById(R.id.textView31);
+	  //    rowssid_2 = (TextView)findViewById(R.id.textView41);
+	    //  rowssid_3 = (TextView)findViewById(R.id.textView47);
+	     // Intent i = new Intent(this, MainActivity.class);
+	     // Intent intent = new Intent(ActivityTwo.this,MainActivity.class).putExtra("myCustomerObj",this);	      
+	      Intent intent_1 = getIntent();
+	      
+	      String fName = intent_1.getStringExtra("FirstRow");
+	      rowssid_1.setText(fName);
+	      Log.d("MY TAG :", "rowssid_1 = " + fName);  
+	      
+	  }
+  
 }
 
 
--- WiFi/src/com/example/wifi/MainActivity.java
+++ WiFi/src/com/example/wifi/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -22,20 +21,20 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+//import ActivityTwo;
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	TextView WifiState,WifiInfo;
 	Button WifiOn,WifiOff,WifiParam;
-	TextView RowTableName,RowSSID_1,RowSSID_2,
-			 RowSSID_3;
+	TextView rowtablename,rowssid_1,rowssid_2,rowssid_3;
 	List<ScanResult> results;
 	ListView lvAP;
 	int extraWifiState ;
-	
-	//String[] apnames = {"              "};
-	final ArrayList<String> apnames = new ArrayList<String>() ;	
 
+	final ArrayList<String> apnames = new ArrayList<String>() ;
+	
+	String myString;
 	  /** Called when the activity is first created. */
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
@@ -46,16 +45,26 @@ public class MainActivity extends Activity implements OnClickListener {
 	      WifiParam = (Button)findViewById(R.id.wifiParam);
 	      WifiState = (TextView)findViewById(R.id.wifiState);
 	      WifiInfo = (TextView)findViewById(R.id.wifiInfo);
-	      RowTableName = (TextView)findViewById(R.id.textTableName);	    
-	      RowSSID_1 = (TextView)findViewById(R.id.textView31);
-	      RowSSID_2 = (TextView)findViewById(R.id.textView41);
-	      RowSSID_3 = (TextView)findViewById(R.id.textView47);
+	      rowtablename = (TextView)findViewById(R.id.textTableName);	    
+	      rowssid_1 = (TextView)findViewById(R.id.textView31);
+	      rowssid_2 = (TextView)findViewById(R.id.textView41);
+	      rowssid_3 = (TextView)findViewById(R.id.textView47);
 	      this.registerReceiver(this.WifiStateChangedReceiver,
 	              new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
 	      WifiOn.setOnClickListener(this);
 	      WifiOff.setOnClickListener(this);
 	      WifiParam.setOnClickListener(this);	
 	      
+	      myString = "HELLO WORLD";
+	      //try {
+	    	  //rowssid_1.setText("MAIN");  
+	    //  } 
+	     // catch (Exception e) {
+	    	//  Log.e("ERROR", "ERROR IN CODE" + e.toString());
+	    	 // e.printStackTrace();
+	      //}	      
+	     // Intent myIntent = new Intent(MainActivity.this, ActivityTwo.class);	      
+     //     myIntent.putExtra("FirstRow",rowssid_1.getText());      
 	      
 	  	  lvAP = (ListView)findViewById(R.id.listAP);
 		  //Create adapter
@@ -69,7 +78,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		public void onClick(View v) {
 			Log.d("MY TAG ","SWITCH on ACTION");		
 			Log.d("MY TAG ", "State = " + extraWifiState);
-			
+
+			//Intent intent_1 = new Intent(this, ActivityTwo.class);
+			Intent intent_1 = new Intent("com.example.wifi");
+		    			
 			WifiManager wifiManager = (WifiManager)getBaseContext().getSystemService(Context.WIFI_SERVICE); 
 			//WifiManager myWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
 			 WifiInfo myWifiInfo = wifiManager.getConnectionInfo();
@@ -91,7 +103,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			case R.id.wifiParam:
 				WifiInfo.setText("BSSID:"     + myWifiInfo.getSSID() + "\n"
 								+ "RSSI:"     + myWifiInfo.getRssi() + "\n"
-								+ "MAC: "     + myWifiInfo.getMacAddress() + "\n"
+							//	+ "MAC: "     + myWifiInfo.getMacAddress() + "\n"
 								+ "LINK SPEED: "     + myWifiInfo.getLinkSpeed() + "\n"
 								+ "SUPPLICANT:" + myWifiInfo.getSupplicantState()
 									);
@@ -110,14 +122,27 @@ public class MainActivity extends Activity implements OnClickListener {
 		            Log.d("MY TAG count = ", "" + count);
 		            switch(count) {
 		            case 0:
-		            	RowSSID_1.setText(result.SSID);
+		            	intent_1.putExtra("FirstRow", myString.toString() );
+		            	//intent_1.putExtra("FirstRow", result.SSID.toString() );
+		            	//intent_1.putExtra("FirstRow", rowssid_1.getText().toString() );
+		            	  //RowSSID1
+		            	startActivity(intent_1);
+		            	//rowssid_1.setText(result.SSID);
+		            	//intent.putExtra("FirstRow", rowssid_1.getText().toString());
+		            	//startActivity(intent);
+		            	
+		            	//rowssid_1.setText(result.SSID);
+		          //      ActivityTwo.rowssid_1.setText("DDDDD");
+		       	     
+		            	
+		             	
 		            	break;
 		            case 1:
-		            	RowSSID_2.setText(result.SSID);
-		            	break;
+		            	//rowssid_2.setText(result.SSID);
+		            //	break;
 		            case 2:
-		            	RowSSID_3.setText(result.SSID);
-		            	break;
+		            	//rowssid_3.setText(result.SSID);
+		            	//break;
 		            }
 		            count++;
 		            apnames.add(result.SSID);    
@@ -172,12 +197,12 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	  public boolean  onOptionsItemSelected(MenuItem item) {
 		    // TODO Auto-generated method stub
-		    switch (item.getItemId()) {
-		    // пункты меню для tvColor
+		    switch (item.getItemId()) {		    
 		    case R.id.switch_screen:
 		    	Log.d("MY TAG ", "Choose item = ");
 		    	Intent intent = new Intent(this, ActivityTwo.class);
-			    startActivity(intent);		    	
+			    startActivity(intent);
+			    //startActivity(intent_1);
 		      //tvColor.setTextColor(Color.RED);
 		      //tvColor.setText("Text color = red");
 		      break;
