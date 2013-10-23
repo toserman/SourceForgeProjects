1a3d126a1b400635e9738dff7e545771b820e095
unknown <anton-nesterenko@ya.ru>
2013-10-23 21:51:40 +0300

    Update scan_list

--- WiFi/src/com/example/wifi/ScanItem.java
+++ WiFi/src/com/example/wifi/ScanItem.java
@@ -3,31 +3,31 @@ package com.example.wifi;
 public class ScanItem {
 	private String ssid;
     private int chanfreq;
-    private int rssi; 
-     
-    public String getSsid() {
+    private int rssi;
+        
+    public String getSsid(){
         return ssid;
     }
  
-    public void setSsid(String ssid) {
+    public void setSsid(String ssid){
         this.ssid = ssid;
     }
  
-    public int getChannelFreq() {
+    public int getChannelFreq(){
         return chanfreq;
     }
-     public void setChannelFreq(int ch_freq) {
+     public void setChannelFreq(int ch_freq){
         this.chanfreq = ch_freq;
     }
-    public int getRSSIlevel() {
+    public int getRSSIlevel(){
         return rssi;
     }
  
-    public void setRSSIlevel(int rssi) {
+    public void setRSSIlevel(int rssi){
         this.rssi = rssi;
     }
- 
-    
+   
+  
     //@Override
     //public String toString() {
       //  return "[ headline=" + headline + ", reporter Name=" +
